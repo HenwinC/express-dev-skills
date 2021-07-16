95ca17f6352fae009b0a87eee442bc60426d49e3
HenwinC <Henwincuff@gmail.com>
2021-07-16 02:29:03 -0500

    lab 1

---
+++ data/skill-db.js
@@ -0,0 +1,58 @@
+export { 
+	find,
+  findById,
+  create,
+  findByIdAndDelete
+}
+
+const skills = [
+  {text: 'HTML', learned: true, _id: 125223},
+  {text: 'JavaScript', learned: true, _id: 127904},
+  {text: 'Css', learned: true, _id: 139608},
+]
+
+function findByIdAndDelete(id, callback) {
+  try { 
+    // Find the index based on the _id of the todo object
+    const idx = skills.findIndex(skill => skill._id == parseInt(id))
+    const deletedSkill = skills.splice(idx, 1)
+    if (!deletedSkill.length ) throw new Error ('No skill was deleted')
+    return callback(null, deletedSkill[0])
+  } catch(error) {
+    return callback(error, null)
+  }
+}
+
+function create(skill, callback) {
+  // Add the id (random)
+  skill._id = Date.now() % 1000000
+  // Set new todos as false
+  skill.done = false
+  console.log(skill)
+  skills.push(skill)
+  return callback(null, skill)
+}
+
+
+const findById = (id, callback) =>{
+  try {
+    const skill = skills.find(skill => skill._id === parseInt(id))
+    if (!skill) throw new Error ('No skill was found')
+    return callback(null, skill)
+  } catch (error) {
+    console.log(error)
+    return callback(error, null)
+  }
+}
+
+const find = (conditions, callback) => {
+  try {
+    if (!(conditions instanceof Object)){
+      throw new TypeError('Please pass in an object')
+    }
+    if (Object.keys(conditions).length === 0) return callback(null, skills)
+  } catch (error) {
+    console.log(error)
+    callback(error, [])
+  }
+}
